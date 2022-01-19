58545159243edba959ab2377059cea00334d7be2
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2022-01-19 04:10:45 -0300

    Added Footer component

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <div class="footer row">
+        <h5>ToDO - Organizando sua vida</h5>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
+
+<style scoped>
+:root {
+    --light: #fff;
+    --dark: #333;
+    --one: #1b2c86;
+    --two: #f4970b;
+    --three: #707070;
+}
+
+.footer {
+    margin: 0;
+    width: 100%;
+    height: 50px;
+    background-color: var(--one);
+    border-top: 7px solid var(--two);
+    position: absolute;
+    bottom: 0;
+    align-items: center;
+}
+
+h5 {
+    color:var(--light);
+    text-align: center;
+    font-weight: bold;
+    font-size: 15px;
+}
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Header/>
+    <Footer/>
   </div>
   
 </template>
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
 export default {
   components:{
     Header,
+    Footer
   }
 }
 </script>
