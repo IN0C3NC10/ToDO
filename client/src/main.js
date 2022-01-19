5d94239cba1cb55537ec35bf76e58c62deb03f5a
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2022-01-19 01:59:30 -0300

    Fixes in client

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Home from './views/Home.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(Home).use(store).use(router).mount('#app')
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Welcome Friend!</h1>
   </div>
+  
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+  color: #2c3e50;
 }
-</script>
+</style>
